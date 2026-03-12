dd11b805b595c6a8e1c1012c7d004cff00f1bae1
syedtirimzi <syedtirimzi@gmail.com>
2026-03-12 21:08:24 +0500

    final for v1

--- src/market-data/market-data.service.ts
+++ src/market-data/market-data.service.ts
@@ -1,3 +1,106 @@
+import { Injectable, Logger, OnModuleInit } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { Exchange } from "@/common/enums/exchanges.enums";
+import { SymbolEntity } from "@/ingestion/symbols/entities/symbol.entity";
+import { BinanceWebSocket } from "@/ingestion/exchanges/binance/binance.ws";
+import { MexcWebSocket } from "@/ingestion/exchanges/mexc/mexc.ws";
+import { MarketEntity } from "./market.entity";
+
+@Injectable()
+export class MarketDataService implements OnModuleInit {
+  private readonly logger = new Logger(MarketDataService.name);
+
+  constructor(
+    @InjectRepository(SymbolEntity)
+    private readonly symbolRepo: Repository<SymbolEntity>,
+    @InjectRepository(MarketEntity)
+        private readonly marketRepo: Repository<MarketEntity>,
+    private readonly binanceWs: BinanceWebSocket,
+    private readonly mexcWs: MexcWebSocket,
+  ) {}
+
+  async onModuleInit() {
+
+    this.logger.log("Starting market WS engine");
+
+    const symbols = await this.symbolRepo.find({
+      relations: ["exchanges", "market"],
+    });
+
+    const symbolMarketMap: Record<string, number> = {};
+    const symbolMetaMap: Record<string, { base: string; quote: string }> = {};
+
+    for (const s of symbols) {
+      for (const ex of s.exchanges) {
+
+        const key = `${ex.exchange}:${s.symbol.toUpperCase()}`;
+
+        symbolMarketMap[key] = s.market.id;
+
+        symbolMetaMap[key] = {
+          base: s.base,
+          quote: s.quote,
+        };
+      }
+    }
+
+    /*
+    ==========================
+    BINANCE SHARDING
+    ==========================
+    */
+
+    const binanceSymbols = symbols
+      .filter(s => s.exchanges.some(e => e.exchange === Exchange.BINANCE))
+      .map(s => s.symbol);
+
+    const BINANCE_CHUNK = 180;
+
+    for (let i = 0; i < binanceSymbols.length; i += BINANCE_CHUNK) {
+
+      const chunk = binanceSymbols.slice(i, i + BINANCE_CHUNK);
+
+      this.logger.log(`Launching Binance WS (${chunk.length} symbols)`);
+
+      this.binanceWs.connect(chunk, symbolMarketMap, symbolMetaMap);
+    }
+
+    /*
+    ==========================
+    MEXC SHARDING
+    ==========================
+    */
+
+    const mexcSymbols = symbols
+      .filter(s => s.exchanges.some(e => e.exchange === Exchange.MEXC))
+      .map(s => s.symbol);
+
+    const MEXC_CHUNK = 25;
+
+    for (let i = 0; i < mexcSymbols.length; i += MEXC_CHUNK) {
+
+      const chunk = mexcSymbols.slice(i, i + MEXC_CHUNK);
+
+      this.logger.log(`Launching MEXC WS (${chunk.length} symbols)`);
+
+      this.mexcWs.connect(chunk, symbolMarketMap, symbolMetaMap);
+    }
+  }
+
+
+  async findBySymbol(symbol: string) {
+
+        return this.marketRepo.findOne({
+          where: {
+            base: 'BTC',
+          },
+        });
+      }
+}
+
+
+
 // import { Injectable, Logger, OnModuleInit } from "@nestjs/common";
 // import { InjectRepository } from "@nestjs/typeorm";
 // import { Repository } from "typeorm";
@@ -5,6 +108,7 @@
 // import { SymbolEntity } from "@/ingestion/symbols/entities/symbol.entity";
 // import { BinanceWebSocket } from "@/ingestion/exchanges/binance/binance.ws";
 // import { MexcWebSocket } from "@/ingestion/exchanges/mexc/mexc.ws";
+// import { MarketEntity } from "./market.entity";
 
 // @Injectable()
 // export class MarketDataService implements OnModuleInit {
@@ -13,6 +117,8 @@
 //   constructor(
 //     @InjectRepository(SymbolEntity)
 //     private readonly symbolRepo: Repository<SymbolEntity>,
+//         @InjectRepository(MarketEntity)
+//     private readonly marketRepo: Repository<MarketEntity>,
 //     private readonly binanceWs: BinanceWebSocket,
 //     private readonly mexcWs: MexcWebSocket,
 //   ) {}
@@ -70,6 +176,16 @@
 //       this.mexcWs.connect(chunk, symbolMarketMap,symbolMetaMap);
 //     }
 //   }
+
+
+//   async findBySymbol(symbol: string) {
+
+//     return this.marketRepo.findOne({
+//       where: {
+//         base: 'BTC',
+//       },
+//     });
+//   }
 // }
 
 
@@ -77,95 +193,194 @@
 
 
 
-import { Injectable, Logger, OnModuleInit } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
-import { Repository } from "typeorm";
-import { Exchange } from "@/common/enums/exchanges.enums";
-import { SymbolEntity } from "@/ingestion/symbols/entities/symbol.entity";
-import { BinanceWebSocket } from "@/ingestion/exchanges/binance/binance.ws";
-import { MexcWebSocket } from "@/ingestion/exchanges/mexc/mexc.ws";
-import { MarketEntity } from "./market.entity";
+// import { Injectable, Logger, OnModuleInit } from "@nestjs/common";
+// import { InjectRepository } from "@nestjs/typeorm";
+// import { Repository } from "typeorm";
+// import { Exchange } from "@/common/enums/exchanges.enums";
+// import { SymbolEntity } from "@/ingestion/symbols/entities/symbol.entity";
+// import { BinanceWebSocket } from "@/ingestion/exchanges/binance/binance.ws";
+// import { MexcWebSocket } from "@/ingestion/exchanges/mexc/mexc.ws";
+// import { MarketEntity } from "./market.entity";
 
-@Injectable()
-export class MarketDataService implements OnModuleInit {
-  private readonly logger = new Logger(MarketDataService.name);
+// @Injectable()
+// export class MarketDataService implements OnModuleInit {
+//   private readonly logger = new Logger(MarketDataService.name);
 
-  constructor(
-    @InjectRepository(SymbolEntity)
-    private readonly symbolRepo: Repository<SymbolEntity>,
+//   constructor(
+//     @InjectRepository(SymbolEntity)
+//     private readonly symbolRepo: Repository<SymbolEntity>,
 
-    @InjectRepository(MarketEntity)
-    private readonly marketRepo: Repository<MarketEntity>,
-    private readonly binanceWs: BinanceWebSocket,
-    private readonly mexcWs: MexcWebSocket,
-  ) {}
+//     @InjectRepository(MarketEntity)
+//     private readonly marketRepo: Repository<MarketEntity>,
+//     private readonly binanceWs: BinanceWebSocket,
+//     private readonly mexcWs: MexcWebSocket,
+//   ) {}
 
-  async onModuleInit() {
-    this.logger.log('Starting canonical market engine…');
+//   async onModuleInit() {
+//     this.logger.log('Starting canonical market engine…');
 
-    const symbols = await this.symbolRepo.find({
-      relations: ['exchanges', 'market'],
-    });
+//     const symbols = await this.symbolRepo.find({
+//       relations: ['exchanges', 'market'],
+//     });
 
-    /**
-     * Map:
-     * BINANCE:ETHUSDT → marketId
-     * MEXC:ETHUSDT → marketId
-     */
+//     /**
+//      * Map:
+//      * BINANCE:ETHUSDT → marketId
+//      * MEXC:ETHUSDT → marketId
+//      */
 
-    const canonicalMarketId = 6262; // BTC-USD market
-
-
-    const symbolMarketMap: Record<string, number> = {};
-    const symbolMetaMap: Record<string, { base: string; quote: string }> = {};
-    const canonicalSymbols = symbols.filter(s =>
-      s.market.id === canonicalMarketId
-    );
-    for (const s of canonicalSymbols) {
-      for (const ex of s.exchanges) {
-        const key = `${ex.exchange}:${s.symbol.toUpperCase()}`;
-        symbolMarketMap[key] = s.market.id;
-        symbolMetaMap[key] = {
-          base: s.base,
-          quote: s.quote,
-        };
-      }
-    }
-
-    // Sharding Binance (max 200 symbols per WS)
-    const binanceSymbols = canonicalSymbols
-      .filter(s => s.exchanges.some(e => e.exchange === Exchange.BINANCE))
-      .map(s => s.symbol);
-
-    const BINANCE_CHUNK = 200;
-    for (let i = 0; i < binanceSymbols.length; i += BINANCE_CHUNK) {
-      const chunk = binanceSymbols.slice(i, i + BINANCE_CHUNK);
-      this.logger.log(`Binance WS symbols: ${chunk.length}`);
-      this.binanceWs.connect(chunk, symbolMarketMap,symbolMetaMap);
-    }
-
-    // Sharding MEXC (max 100 symbols per WS)
-    const mexcSymbols = canonicalSymbols
-      .filter(s => s.exchanges.some(e => e.exchange === Exchange.MEXC))
-      .map(s => s.symbol);
-
-    const MEXC_CHUNK = 100;
-    for (let i = 0; i < mexcSymbols.length; i += MEXC_CHUNK) {
-      const chunk = mexcSymbols.slice(i, i + MEXC_CHUNK);
-      this.logger.log(`MEXC WS symbols: ${chunk.length}`);
-      this.mexcWs.connect(chunk, symbolMarketMap,symbolMetaMap);
-    }
-  }
+//     const canonicalMarketId = 6262; // BTC-USD market
 
 
-  async findBySymbol(symbol: string) {
+//     const symbolMarketMap: Record<string, number> = {};
+//     const symbolMetaMap: Record<string, { base: string; quote: string }> = {};
+//     const canonicalSymbols = symbols.filter(s =>
+//       s.market.id === canonicalMarketId
+//     );
+//     for (const s of canonicalSymbols) {
+//       for (const ex of s.exchanges) {
+//         const key = `${ex.exchange}:${s.symbol.toUpperCase()}`;
+//         symbolMarketMap[key] = s.market.id;
+//         symbolMetaMap[key] = {
+//           base: s.base,
+//           quote: s.quote,
+//         };
+//       }
+//     }
 
-    return this.marketRepo.findOne({
-      where: {
-        base: 'BTC',
-      },
-    });
-  }
+//     // Sharding Binance (max 200 symbols per WS)
+//     const binanceSymbols = canonicalSymbols
+//       .filter(s => s.exchanges.some(e => e.exchange === Exchange.BINANCE))
+//       .map(s => s.symbol);
+
+//     const BINANCE_CHUNK = 200;
+//     for (let i = 0; i < binanceSymbols.length; i += BINANCE_CHUNK) {
+//       const chunk = binanceSymbols.slice(i, i + BINANCE_CHUNK);
+//       this.logger.log(`Binance WS symbols: ${chunk.length}`);
+//       this.binanceWs.connect(chunk, symbolMarketMap,symbolMetaMap);
+//     }
+
+//     // Sharding MEXC (max 100 symbols per WS)
+//     const mexcSymbols = canonicalSymbols
+//       .filter(s => s.exchanges.some(e => e.exchange === Exchange.MEXC))
+//       .map(s => s.symbol);
+
+//     const MEXC_CHUNK = 100;
+//     for (let i = 0; i < mexcSymbols.length; i += MEXC_CHUNK) {
+//       const chunk = mexcSymbols.slice(i, i + MEXC_CHUNK);
+//       this.logger.log(`MEXC WS symbols: ${chunk.length}`);
+//       this.mexcWs.connect(chunk, symbolMarketMap,symbolMetaMap);
+//     }
+//   }
 
 
-}
+//   async findBySymbol(symbol: string) {
+
+//     return this.marketRepo.findOne({
+//       where: {
+//         base: 'BTC',
+//       },
+//     });
+//   }
+
+
+// }
+
+
+
+
+
+
+
+
+// import { Injectable, Logger, OnModuleInit } from "@nestjs/common";
+// import { InjectRepository } from "@nestjs/typeorm";
+// import { Repository } from "typeorm";
+// import { Exchange } from "@/common/enums/exchanges.enums";
+// import { SymbolEntity } from "@/ingestion/symbols/entities/symbol.entity";
+// import { BinanceWebSocket } from "@/ingestion/exchanges/binance/binance.ws";
+// import { MexcWebSocket } from "@/ingestion/exchanges/mexc/mexc.ws";
+// import { MarketEntity } from "./market.entity";
+
+// @Injectable()
+// export class MarketDataService implements OnModuleInit {
+//   private readonly logger = new Logger(MarketDataService.name);
+
+//   constructor(
+//     @InjectRepository(SymbolEntity)
+//     private readonly symbolRepo: Repository<SymbolEntity>,
+
+//     @InjectRepository(MarketEntity)
+//     private readonly marketRepo: Repository<MarketEntity>,
+//     private readonly binanceWs: BinanceWebSocket,
+//     private readonly mexcWs: MexcWebSocket,
+//   ) {}
+
+//   async onModuleInit() {
+//     this.logger.log('Starting canonical market engine…');
+
+//     const symbols = await this.symbolRepo.find({
+//       relations: ['exchanges', 'market'],
+//     });
+
+//     /**
+//      * Map:
+//      * BINANCE:ETHUSDT → marketId
+//      * MEXC:ETHUSDT → marketId
+//      */
+
+
+
+// //     const symbolMarketMap: Record<string, number> = {};
+// //     const symbolMetaMap: Record<string, { base: string; quote: string }> = {};
+// //     const canonicalSymbols = symbols.filter(s =>
+// //       s.market.id === canonicalMarketId
+// //     );
+// //     for (const s of canonicalSymbols) {
+// //       for (const ex of s.exchanges) {
+// //         const key = `${ex.exchange}:${s.symbol.toUpperCase()}`;
+// //         symbolMarketMap[key] = s.market.id;
+// //         symbolMetaMap[key] = {
+// //           base: s.base,
+// //           quote: s.quote,
+// //         };
+// //       }
+// //     }
+
+// //     // Sharding Binance (max 200 symbols per WS)
+// //     const binanceSymbols = canonicalSymbols
+// //       .filter(s => s.exchanges.some(e => e.exchange === Exchange.BINANCE))
+// //       .map(s => s.symbol);
+
+// //     const BINANCE_CHUNK = 200;
+// //     for (let i = 0; i < binanceSymbols.length; i += BINANCE_CHUNK) {
+// //       const chunk = binanceSymbols.slice(i, i + BINANCE_CHUNK);
+// //       this.logger.log(`Binance WS symbols: ${chunk.length}`);
+// //       this.binanceWs.connect(chunk, symbolMarketMap,symbolMetaMap);
+// //     }
+
+// //     // Sharding MEXC (max 100 symbols per WS)
+// //     const mexcSymbols = canonicalSymbols
+// //       .filter(s => s.exchanges.some(e => e.exchange === Exchange.MEXC))
+// //       .map(s => s.symbol);
+
+// //     const MEXC_CHUNK = 100;
+// //     for (let i = 0; i < mexcSymbols.length; i += MEXC_CHUNK) {
+// //       const chunk = mexcSymbols.slice(i, i + MEXC_CHUNK);
+// //       this.logger.log(`MEXC WS symbols: ${chunk.length}`);
+// //       this.mexcWs.connect(chunk, symbolMarketMap,symbolMetaMap);
+// //     }
+// //   }
+
+
+// //   async findBySymbol(symbol: string) {
+
+// //     return this.marketRepo.findOne({
+// //       where: {
+// //         base: 'BTC',
+// //       },
+// //     });
+// //   }
+
+
+// // }
